0803c0342e4a020f3eeb1a04373a5fb2148dc8dd
leem0n31 <macsclepcha@gmail.com>
2025-11-13 14:17:49 +0300

    API_Project

---
+++ BusinessLogic/Services/UserService.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Interfaces;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessLogic.Services
+{
+    public class UserService : IUserService
+    {
+        private IRepositoryWrapper _repositoryWrapper;
+
+        public UserService(IRepositoryWrapper repositoryWrapper)
+        {
+            _repositoryWrapper = repositoryWrapper;
+        }
+
+        public async Task<List<Пользователи>> GetAll()
+        {
+            return await _repositoryWrapper.Пользователи.FindAll();
+        }
+
+        public async Task<Пользователи> GetById(int id)
+        {
+            var user = await _repositoryWrapper.Пользователи
+                .FindByCondition(x => x.IdПользователя == id);
+            return user.First();
+        }
+
+        public async Task Create(Пользователи model)
+        {
+            await _repositoryWrapper.Пользователи.Create(model);
+            _repositoryWrapper.Save();
+        }
+
+        public async Task Update(Пользователи model)
+        {
+            _repositoryWrapper.Пользователи.Update(model);
+            _repositoryWrapper.Save();
+        }
+
+        public async Task Delete(int id)
+        {
+            var user = await _repositoryWrapper.Пользователи
+                .FindByCondition(x => x.IdПользователя == id);
+
+            _repositoryWrapper.Пользователи.Delete(user.First());
+            _repositoryWrapper.Save();
+        }
+    }
+}
